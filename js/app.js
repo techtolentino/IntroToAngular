8adb8f911de7c6bb755a631a65e6834dbd4c9401
Randy Tolentino <makertolentino@gmail.com>
2015-02-22 06:15:11 -0600

    starting fresh

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('app', []).config(function($routeProvider){
+		$routeProvider.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'LoginController'
+		});
+
+		$routeProvider.otherwise( {redirectTo : '/login'});
+	});
+
+app.controller('LoginController', function(){
+
+});
