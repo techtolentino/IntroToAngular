54bfe639aceb27f9bc14df1be05edcd0b7808173
Randy Tolentino <makertolentino@gmail.com>
2015-02-22 20:22:50 -0600

    added template folder and new home route

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 var app = angular.module('app', ['ngRoute']).config(function($routeProvider){
 		$routeProvider.when('/login', {
-			templateUrl: 'login.html',
+			templateUrl: 'templates/login.html',
 			controller: 'LoginController'
+		});
+
+		$routeProvider.when('/home', {
+			templateUrl: 'templates/home.html',
+			controller: 'HomeController'
 		});
 
 		$routeProvider.otherwise( {redirectTo : '/login'});
 	});
 
 app.controller('LoginController', function(){
+
+});
+
+app.controller('HomeController', function(){
 
 });
