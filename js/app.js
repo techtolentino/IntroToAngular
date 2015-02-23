b7a3ab0d114478d140dc10ee90d30f6b331ca517
Randy Tolentino <makertolentino@gmail.com>
2015-02-23 04:28:59 -0600

    adding skeleton

--- js/app.js
+++ js/app.js
@@ -12,10 +12,16 @@ var app = angular.module('app', ['ngRoute']).config(function($routeProvider){
 		$routeProvider.otherwise( {redirectTo : '/login'});
 	});
 
-app.controller('LoginController', function($scope){
+app.controller('LoginController', function($scope, $location){
 	$scope.credentials = { username: "", password: ""};
+
+	$scope.login = function() {
+		if($scope.credentials.username === 'randy') {
+			$location.path('/home');
+		}
+	};
 });
 
 app.controller('HomeController', function(){
-
+	
 });
